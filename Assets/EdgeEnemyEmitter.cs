40485ad92edadf3dd5a599bc86c58da750592bad
SSSClassGhoul <150183887+SSSClassGhoul@users.noreply.github.com>
2023-12-04 06:07:01 +0300

    Lvl1 proto

---
+++ Assets/EdgeEnemyEmitter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeEnemyEmitter : MonoBehaviour
+{
+    public GameObject enemy;
+    public float minDelay, maxDelay;
+
+    float nextLaunchTime;
+
+    void Update()
+    {
+        if (!GCScript.instance.isStarted)
+        {
+            return;
+        }
+        if (Time.time > nextLaunchTime)
+        {
+            var posY = 0;
+            var posZ = Random.Range(-transform.localScale.z / 2 + transform.position.z, transform.localScale.z / 2 + transform.position.z);
+            var posX =  transform.position.z;
+            Instantiate(enemy, new Vector3(posX, posY, posZ), Quaternion.identity);
+            nextLaunchTime = Time.time + Random.Range(minDelay, maxDelay);
+        }
+    }
+
+}
